05960313e349e3215e67a399c885e350a7005c84
Nemo505 <dahliahalesia8@gmail.com>
2022-09-29 13:33:28 +0630

    rightBar

--- src/components/rightbar/Rightbar.jsx
+++ src/components/rightbar/Rightbar.jsx
@@ -1,7 +1,30 @@
 import "./rightbar.css"
+import { Cake } from "@mui/icons-material";
 
 export default function Rightbar() {
   return (
-    <div className='rightbar'>Rightbar</div>
+    <div className='rightbar'>
+      <div className="rightbarWrapper">
+        <div className="birthdayContainer">
+          <Cake htmlColor="olive" className="birthdayImg"/>
+          <span className="birthdayText">
+           <b> LeoWu </b>and <b>Chyu</b> have Birthdays Today.</span>
+        </div>
+
+        <img src="assets/maple/wu3.jpg" className="rightbarAd" alt="" />
+        <h4 className="rightbarTitle">OnlineFriend</h4>
+
+        <ul className="rightbarFriendList">
+          <li className="rightbarFriend">
+              <div className="rightbarFriendContainer">
+                <img src="assets/maple/wu2.jpg" className="rightbarFriendImg" alt="" />
+                <span className="rightbarOnline"></span>
+              </div>
+                <span className="rightbarUsername">Nemo</span>
+          </li>
+        </ul>
+
+      </div>
+    </div>
   )
 }
