0f94eb19f47e4e137613187ac4a0b959282132da
Nemo505 <dahliahalesia8@gmail.com>
2022-10-17 09:56:14 +0630

    logIn

---
+++ src/pages/login/Login.jsx
@@ -0,0 +1,17 @@
+import "./login.css"
+
+export default function Login() {
+  return (
+    <div className="login">
+        <div className="loginWrapper">
+            <div className="loginLeft">
+                <h3 className="loginLogo">Maple Social</h3>
+                <span className="loginDesc">
+                    Connect With people around the World
+                </span>
+            </div>
+            <div className="loginRight"></div>
+        </div>
+    </div>
+  )
+}
